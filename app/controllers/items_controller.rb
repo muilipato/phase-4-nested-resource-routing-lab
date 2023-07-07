7ab2c3e721e691cab27fae79d0cae7e247923c88
Patrick <muilipato@gmail.com>
2023-07-07 19:11:18 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items
   end
+
+    def show
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+      render json: item
+    end
+
+    def create
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      render json: item
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :price)
+    end
 
 end
